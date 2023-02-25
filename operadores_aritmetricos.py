02f0ca74916edfb717a441f79197b0041f1ce2f8
Zeukodeus <carlos2234503@correo.uis.edu.co>
2023-02-25 08:26:04 -0500

    Operadores_Aritmetricos

---
+++ operadores_aritmetricos.py
@@ -0,0 +1,25 @@
+print("---------------------------")
+print("--operadores aritmetricos--")
+print("---------------------------")
+
+x = int(input("digite el valor de x: "))
+y = int(input("digite el valor de y: "))
+
+s = x + y
+r = x - y
+m = x * y
+d = x / y
+mod = x % y
+de = x // y
+p = x ** y
+
+print("la suma es: " + str(s))
+print("la resta es: " + str(r))
+print("la multiplicacion es: " + str(m))
+print("la division es: " + str(d))
+print("el modulo es: " + str(mod))
+print("la division entera es: " + str(de))
+print("la potencia es: " + str(p))
+print("--------------------------")
+print("-----fin del programa-----")
+print("--------------------------")
